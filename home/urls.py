d6389ab6012c85271328fa21a17b6ed6156a4f68
MasayoshiKato <masa_yoshi_basket@icloud.com>
2025-10-02 20:10:30 +0900

    First Commit

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+app_name = "diary"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("page/create/", views.page_create, name="page_create"),
+    path("pages/", views.page_list, name="page_list"),
+    path("pages/<uuid:id>/", views.page_detail, name="page_detail"),
+]
---
+++ home/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from .forms import PageForm
+from .forms import Page
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
+class IndexView(View):
+    def get(self, request):
+        datetime_now = datetime.now(
+            ZoneInfo("Asia/Tokyo")
+        ).strftime("%Y年%m月%d日 %H:%M:%S")
+        return render(request, "home/index.html", {"datetime_now": datetime_now})
+    
+class PageCreateView(View):
+    def get(self, request):
+        form = PageForm()
+        return render(request, "home/page_form.html", {"form": form})
+    
+    def post(self, request):
+        form = PageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("diary:index")
+        return render(request, "home/page_form.html", {"form": form})
+    
+class PageListView(View):
+    def get(self, request):
+        page_list = Page.objects.order_by("page_date")
+        return render(request, "home/page_list.html", {"page_list": page_list})
+    
+class PageDetailView(View):
+    def get(self, request, id):
+        page = get_object_or_404(Page, id=id)
+        return render(request, "home/page_detail.html", {"page": page})
+        
+
+
+index = IndexView.as_view()
+page_create = PageCreateView.as_view()
+page_list = PageListView.as_view()
+page_detail = PageDetailView.as_view()
